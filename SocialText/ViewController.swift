a361ddfdb0ca6b433036702a0d34c597f76a469e
Mohammad Shayan <mohammadhemani@gmail.com>
2020-05-10 19:13:00 +0430

    Initial Commit

---
+++ SocialText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialText
+//
+//  Created by Mohammad Shayan on 5/10/20.
+//  Copyright © 2020 Mohammad Shayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
